ce06650e284263efc98a65dc4180cb7e4ae11deb
Jovanie Dasian <DASI20248151@MLHUILLIER.LOCAL>
2024-03-01 15:40:27 +0800

    adding new commit for the dev branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static void vanie(){
+        System.out.println("Hello world!");
+    }
+    }
 }
